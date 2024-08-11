d01ac4de43932a23e0da2916e2652b6f9f920d54
shruti <shruti.grover2001@gmail.com>
2024-08-11 13:48:08 +0530

    Next commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const num = 10;
+const str = "Hello";
+const bool = true;
+const undef = undefined;
+const nu = null;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof undef);
+console.log(typeof nu);
