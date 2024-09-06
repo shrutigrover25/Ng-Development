7a62993b344ff66fe30c38f2b6ec169417eda44b
shruti <shruti.grover2001@gmail.com>
2024-09-06 18:23:51 +0530

    chnges

--- Ques1.js
+++
@@ -1,5 +0,0 @@
-// 1. Write a js code to reverse a string
-// 2. Remove a specific character from string
-// 3. Check if the string only contains the alphabets
-// 4. Given a string, find out the occurence of any character in a string
-// 5.Make the first character of every String as the upperCase
